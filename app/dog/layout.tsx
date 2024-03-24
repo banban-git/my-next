de27318b9b67f6ab79badd40435bcc34f5d1fc73
banban-git <runaway_ban2@ypost.plala.or.jp>
2024-03-24 21:17:08 +0900

    dog

--- app/dog/layout.tsx
+++ app/dog/layout.tsx
@@ -3,6 +3,11 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const metadata: Metadata = {
+  title: "犬の画像",
+  description: "犬の画像取得",
+};
+
 // https://en-ambi.com/itcontents/entry/2023/04/18/093000/
 export default function RootLayout({
   children,
